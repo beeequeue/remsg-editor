166e81bbc80b34531e22ded35898703b9d8204f9
Adam <adam@haglund.dev>
2024-10-31 18:44:41 +0100

    add service worker for offline usage, split bundle

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,31 @@
 import path from "node:path"
 
 import { defineConfig } from "vite"
+import { VitePWA } from "vite-plugin-pwa"
 
 import inject from "@rollup/plugin-inject"
 import { svelte } from "@sveltejs/vite-plugin-svelte"
 import unocss from "@unocss/vite"
 
-export default defineConfig(({}) => ({
-  build: { target: "firefox125" },
+const moduleNameRegex = /\/node_modules\/(?!\.pnpm)(.+?)\//
+
+export default defineConfig({
+  build: {
+    target: "firefox125",
+    rollupOptions: {
+      output: {
+        manualChunks: (id, { getModuleInfo }) => {
+          if (id.includes("node_modules")) {
+            if ((getModuleInfo(id)?.code?.length ?? 0) < 1000) {
+              return "vendor"
+            }
+
+            return moduleNameRegex.exec(id)?.[1] ?? "vendor"
+          }
+        },
+      },
+    },
+  },
 
   resolve: {
     alias: {
@@ -16,5 +34,26 @@ export default defineConfig(({}) => ({
     },
   },
 
-  plugins: [svelte(), unocss(), inject({ Buffer: ["buffer"] })],
-}))
+  plugins: [
+    svelte(),
+    unocss(),
+    inject({ Buffer: ["buffer"] }) as never,
+    VitePWA({
+      registerType: "prompt",
+      injectRegister: "script-defer",
+      manifest: {
+        name: "REMsg Editor",
+        theme_color: "#222",
+        display: "minimal-ui",
+        orientation: "portrait",
+        icons: [
+          {
+            src: "/icon.png",
+            sizes: "256x256",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+})
